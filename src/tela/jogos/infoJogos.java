59ddfdd77d84bd3f4ab20d6d38aaac24041f5a1b
Gabriel Oliveira <gdravk@outlook.com>
2024-03-13 02:17:51 -0300

    adicionado funções, imagens e links de trailer

--- src/tela/jogos/infoJogos.java
+++ src/tela/jogos/infoJogos.java
@@ -5,20 +5,41 @@ import java.util.List;
 
 public class infoJogos {
     private List<Jogo> jogos;
+    private List<String> urls; // Adicione uma lista para armazenar as URLs dos jogos
 
     public infoJogos() {
         jogos = new ArrayList<>();
+        urls = new ArrayList<>();
 
         // Adiciona os jogos
-        jogos.add(new Jogo(" League of Legends", "MOBA", "caminho/do/icone1.png"));
-        jogos.add(new Jogo(" Fortnite", "Battle Royale FPS", "caminho/do/icone2.png"));
-        jogos.add(new Jogo(" Stardew Valley", "Farming simulation", "caminho/do/icone3.png"));
-        jogos.add(new Jogo(" Palworld", "RPG", "caminho/do/icone4.png"));
-        jogos.add(new Jogo(" Elden Ring", "Soulslike", "caminho/do/icone5.png"));
-        jogos.add(new Jogo(" Minecraft", "Simulation", "caminho/do/icone6.png"));
+        jogos.add(new Jogo(" League of Legends", "MOBA", "src/tela/imagens/logoJogos/league_of_legends_logo.png"));
+        urls.add("https://youtu.be/aR-KAldshAE?si=6IjPbBbcMTxMNeVe");
+
+        jogos.add(new Jogo(" Fortnite", "Battle Royale FPS", "src/tela/imagens/logoJogos/fortnite_logo.jpeg"));
+        urls.add("https://youtu.be/eHBN4oBAwiA?si=CpfK58ZEdvQynYEu");
+
+        jogos.add(new Jogo(" Stardew Valley", "Farming simulation",
+                "src/tela/imagens/logoJogos/stardew_valley_logo.jpg"));
+        urls.add("https://youtu.be/ot7uXNQskhs?si=BnyQelVVkZTAUOcm");
+
+        jogos.add(new Jogo(" Palworld", "RPG", "src/tela/imagens/logoJogos/palworld_logo.jpg"));
+        urls.add("https://youtu.be/D9w97KSEAOo?si=3ONTsXTm6Y6dUCLV");
+
+        jogos.add(new Jogo(" Elden Ring", "Soulslike", "src/tela/imagens/logoJogos/elden_ring_logo.jpg"));
+        urls.add("https://youtu.be/E3Huy2cdih0?si=laIKCzBWt55fsv95");
+
+        jogos.add(new Jogo(" Minecraft", "Sandbox Simulation", "src/tela/imagens/logoJogos/minecraft_logo.jpg"));
+        urls.add("https://youtu.be/Rla3FUlxJdE?si=_PlD3U1BFC5i93nU");
+
     }
 
     public List<Jogo> getJogos() {
         return jogos;
     }
+
+    // Método para obter a URL correspondente ao jogo no índice especificado
+    public String getUrl(int index) {
+        return urls.get(index);
+    }
+    
 }
--- src/tela/tela.java
+++ src/tela/tela.java
@@ -2,30 +2,33 @@ package tela;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import tela.jogos.*;
 
 public class tela extends JFrame {
     private CardLayout cardLayout;
     private JPanel cardPanel;
+    private String urlSelecionada;
+    private JButton botaoSelecionado; // Novo atributo para armazenar o botão selecionado
 
     public tela() {
         super("Java Games Launcher");
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 400);
+        setSize(650, 550);
 
-        // Criação do CardLayout para conter os cartões da segunda tela de jogos
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
-        add(cardPanel); // Adiciona o painel ao JFrame
+        add(cardPanel);
 
-        // Primeira tela com o GIF como plano de fundo
         JPanel primeiraTela = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                ImageIcon gifIcon = new ImageIcon("src/tela/backgroundTela/background_primeira_tela.gif");
+                ImageIcon gifIcon = new ImageIcon("src/tela/imagens/backgroundTela/background_primeira_tela.gif");
                 g.drawImage(gifIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
             }
         };
@@ -34,42 +37,81 @@ public class tela extends JFrame {
         JLabel tituloLabel = new JLabel("Launcher de jogos!");
         tituloLabel.setHorizontalAlignment(SwingConstants.CENTER);
         tituloLabel.setVerticalAlignment(SwingConstants.CENTER);
-        tituloLabel.setForeground(Color.WHITE); // Cor vermelha para destacar
-        tituloLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 48)); // Fonte em negrito e tamanho aumentado
+        tituloLabel.setForeground(Color.WHITE);
+        tituloLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 48));
         primeiraTela.add(tituloLabel, BorderLayout.CENTER);
 
         JButton botaoIniciarLauncher = new JButton("Iniciar Launcher");
         botaoIniciarLauncher.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cardLayout.next(cardPanel); // Quando clica em iniciar launcher, abre o próximo cartão de tela que mostra os jogos
+                cardLayout.next(cardPanel);
             }
         });
         primeiraTela.add(botaoIniciarLauncher, BorderLayout.SOUTH);
 
         cardPanel.add(primeiraTela, "primeiraTela");
 
-        // Implementação da segunda tela (tela dos jogos)
         JPanel segundaTela = new JPanel();
         segundaTela.setLayout(new BorderLayout());
 
-        JLabel labelJogos = new JLabel("Lista de Jogos");
-        labelJogos.setHorizontalAlignment(SwingConstants.CENTER);
-        labelJogos.setVerticalAlignment(SwingConstants.CENTER);
-        segundaTela.add(labelJogos, BorderLayout.CENTER);
+        JButton botaoJogar = new JButton("Assistir Trailer");
+        botaoJogar.setFont(new Font("Arial", Font.BOLD, 14));
+        Color verdeFolha = new Color(51, 153, 51);
+        botaoJogar.setBackground(verdeFolha);
+        botaoJogar.setForeground(Color.WHITE);
+        botaoJogar.setFocusPainted(false);
+        botaoJogar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (urlSelecionada != null) {
+                    try {
+                        abrirLink(urlSelecionada);
+                    } catch (IOException | URISyntaxException ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Por favor, selecione um jogo antes de jogar.");
+                }
+            }
+        });
+        segundaTela.add(botaoJogar, BorderLayout.NORTH);
+
+        infoJogos info = new infoJogos();
+        JPanel panelJogos = new JPanel(new GridLayout(2, 3));
+        for (int i = 0; i < info.getJogos().size(); i++) {
+            Jogo jogo = info.getJogos().get(i);
+            JButton botaoJogo = new JButton();
+            botaoJogo.setIcon(new ImageIcon(jogo.getCaminhoIcone()));
+            botaoJogo.setToolTipText(jogo.getNome() + "\n" + jogo.getDescricao());
+            String url = info.getUrl(i);
+            botaoJogo.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    urlSelecionada = url;
+                    if (botaoSelecionado != null) {
+                        botaoSelecionado.setBackground(null); // Remova o destaque do botão anteriormente selecionado
+                    }
+                    botaoSelecionado = (JButton) e.getSource(); // Armazene o botão atualmente selecionado
+                    botaoSelecionado.setBackground(new Color(161, 159, 183));//quando clica no botao de selecionar o jogo fica com essa cor rgb cinza azulado no fundo
+                }
+            });
+            panelJogos.add(botaoJogo);
+        }
+        segundaTela.add(panelJogos, BorderLayout.CENTER);
 
         JButton botaoVoltar = new JButton("Voltar");
         botaoVoltar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                cardLayout.previous(cardPanel); // Quando clicado, volta para a tela inicial
+                cardLayout.previous(cardPanel);
             }
         });
         segundaTela.add(botaoVoltar, BorderLayout.SOUTH);
 
         cardPanel.add(segundaTela, "segundaTela");
 
-        cardLayout.show(cardPanel, "primeiraTela"); // Define qual cartão será exibido inicialmente
+        cardLayout.show(cardPanel, "primeiraTela");
     }
 
     public static void main(String[] args) {
@@ -80,5 +122,13 @@ public class tela extends JFrame {
                 telaPrincipal.setVisible(true);
             }
         });
+    }
+
+    private void abrirLink(String url) throws IOException, URISyntaxException {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(new URI(url));
+        } else {
+            JOptionPane.showMessageDialog(null, "Navegador não suportado.");
+        }
     }
 }
